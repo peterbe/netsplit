3a6d4457e379387776b6f1b1ae59b53d1b5978df
Peter Bengtsson <mail@peterbe.com>
2013-01-19 14:15:16 -0800

    tests pass

---
+++ app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import json
+
+from flask import Flask, abort, make_response, request
+app = Flask(__name__)
+
+
+STATE = {}
+
+def add_debt(from_, to, amount):
+    if from_ > to:
+        amount *= -1
+        from_, to = to, from_
+    tup = (from_, to)
+    STATE[tup] = STATE.get(tup, 0) + amount
+
+
+def serialize(from_, to, amount):
+    return {"from": from_, "to": to, "amount": amount}
+
+
+@app.route("/event", methods=['POST'])
+def event():
+    data = json.loads(request.data)
+    for debt in data:
+        add_debt(debt['from'], debt['to'], debt['amount'])
+
+    current_debts = []
+    for (from_, to), amount in STATE.items():
+        if amount < 0:
+            from_, to = to, from_
+            amount *= -1
+        row = serialize(from_, to, amount)
+        current_debts.append(row)
+
+    return json.dumps(current_debts)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
---
+++ tests/test_app.py
@@ -0,0 +1,101 @@
+import os
+import unittest
+import json
+import tempfile
+
+import app
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        #self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        #flaskr.init_db()
+        app.STATE = {}
+
+    def tearDown(self):
+        app.STATE = {}
+        #os.close(self.db_fd)
+        #os.unlink(flaskr.app.config['DATABASE'])
+
+    def test_posting_event(self):
+        event_data = [
+          {'from': 'E', 'to': 'P', 'amount': 10},
+        ]
+        response = self.app.post('/event', data=json.dumps(event_data))
+        state_data = json.loads(response.data)
+        assert state_data[0]['from'] == 'E'
+        assert state_data[0]['to'] == 'P'
+        assert state_data[0]['amount'] == 10
+
+        event_data = [
+            {'from': 'P', 'to': 'E', 'amount': 7},
+        ]
+        response = self.app.post('/event', data=json.dumps(event_data))
+        state_data = json.loads(response.data)
+        assert state_data[0]['from'] == 'E'
+        assert state_data[0]['to'] == 'P'
+        assert state_data[0]['amount'] == 3
+
+    def test_scenario1(self):
+        event_data = [
+            app.serialize('E', 'P', 10),
+            app.serialize('L', 'P', 10),
+            app.serialize('M', 'P', 5),
+        ]
+        response = self.app.post('/event', data=json.dumps(event_data))
+        state_data = json.loads(response.data)
+
+        assert app.serialize('E', 'P', 10) in state_data
+        assert app.serialize('L', 'P', 10) in state_data
+        assert app.serialize('M', 'P', 5) in state_data
+
+        # 2
+        event_data = [
+            app.serialize('E', 'M', 10),
+            app.serialize('L', 'M', 10),
+        ]
+        response = self.app.post('/event', data=json.dumps(event_data))
+        state_data = json.loads(response.data)
+
+        assert app.serialize('E', 'M', 10) in state_data
+        assert app.serialize('L', 'M', 10) in state_data
+        assert app.serialize('E', 'P', 10) in state_data
+        assert app.serialize('L', 'P', 10) in state_data
+        assert app.serialize('M', 'P', 5) in state_data
+
+        # 3
+        event_data = [
+            app.serialize('P', 'L', 12),
+            app.serialize('M', 'L', 12),
+        ]
+        response = self.app.post('/event', data=json.dumps(event_data))
+        state_data = json.loads(response.data)
+
+        assert app.serialize('P', 'L', 2) in state_data
+        assert app.serialize('M', 'L', 2) in state_data
+        assert app.serialize('E', 'P', 10) in state_data
+        assert app.serialize('E', 'M', 10) in state_data
+        assert app.serialize('M', 'P', 5) in state_data
+
+        # 4
+        event_data = [
+            app.serialize('P', 'E', 7),
+            app.serialize('L', 'E', 7),
+            app.serialize('M', 'E', 7),
+        ]
+        response = self.app.post('/event', data=json.dumps(event_data))
+        state_data = json.loads(response.data)
+
+        assert app.serialize('E', 'P', 3) in state_data
+        assert app.serialize('L', 'E', 7) in state_data
+        assert app.serialize('E', 'M', 3) in state_data
+        assert app.serialize('P', 'L', 2) in state_data
+        assert app.serialize('M', 'L', 2) in state_data
+        assert app.serialize('M', 'P', 5) in state_data
+
+
+if __name__ == '__main__':
+    unittest.main()
